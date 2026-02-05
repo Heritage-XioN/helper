2105cfa84af07e3b449fce9ad7f75861f91374de
HERITAGE-XION <iheritage934@gmail.com>
2026-02-05 14:20:43 +0100

    removed scroll-bar from page

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+				className={`${geistSans.variable} ${geistMono.variable} antialiased no-scrollbar`}
 			>
 				<main className='w-full pt-16'>
 					<Navbar />
